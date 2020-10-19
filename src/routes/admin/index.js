b99b162a5ecdb41c72e9d883151f4c7dbb8e6d2b
woadudakand <alwaysitlab@gmail.com>
2020-10-19 17:07:31 +0600

    commit change

--- src/routes/admin/index.js
+++ src/routes/admin/index.js
@@ -14,15 +14,14 @@ const Projects = lazy(() => import('./projects'));
 const Calendars = lazy(() => import('../../container/Calendar'));
 const Tables = lazy(() => import('../../container/table/Table'));
 const Forms = lazy(() => import('../../container/forms/Forms'));
-
 const Inbox = lazy(() => import('../../container/email/Email'));
 const Chat = lazy(() => import('../../container/chat/ChatApp'));
 const Settings = lazy(() => import('../../container/profile/settings/Settings'));
+const Firebase = lazy(() => import('./firebase'));
 const Myprofile = lazy(() => import('../../container/profile/myProfile/Index'));
 const ToDo = lazy(() => import('../../container/toDo/ToDo'));
 const Note = lazy(() => import('../../container/note/Note'));
 const Contact = lazy(() => import('../../container/contact/Contact'));
-const Calendar = lazy(() => import('../../container/calendar/Calendar'));
 
 const Admin = () => {
   const { path } = useRouteMatch();
@@ -50,13 +49,13 @@ const Admin = () => {
         <Route path={`${path}/forms`} component={Forms} />
 
         <Route path={`${path}/email/:page`} component={Inbox} />
+        <Route path={`${path}/firestore`} component={Firebase} />
         <Route path={`${path}/main/chat`} component={Chat} />
         <Route path={`${path}/profile/settings`} component={Settings} />
         <Route path={`${path}/profile/myProfile`} component={Myprofile} />
         <Route path={`${path}/app/to-do`} component={ToDo} />
         <Route path={`${path}/app/note`} component={Note} />
         <Route path={`${path}/app/contact`} component={Contact} />
-        <Route path={`${path}/app/calendar`} component={Calendar} />
       </Suspense>
     </Switch>
   );
